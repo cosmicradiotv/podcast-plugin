ded17c2d6263902616fdc20c0310908dcae805e9
Matthew Fraser <blendermf@gmail.com>
2015-04-15 05:34:23 -0400

    Initial Plugin Commit
    This is just a basic OctoberCMS plugin at the moment, it currently does
    nothing.

---
+++ Plugin.php
@@ -0,0 +1,26 @@
+<?php namespace CosmicRadioTV\U62;
+
+use System\Classes\PluginBase;
+
+/**
+ * u62 Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'u62',
+            'description' => 'No description provided yet...',
+            'author'      => 'CosmicRadioTV',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# u62
-Podcast
+# [u62](https://github.com/cosmicradiotv/u62) #
+Podcast network plugin for OctoberCMS
+
+Currently this is literally the default plugin that is created through artisan in octobercms. There is much work to be done.
+
+## TODO ##
+Everything. Including this README, with a much better description of what this project is.
