399fdeffff32c37093a4c1a4249630ec38ac4c43
Matthew Fraser <blendermf@gmail.com>
2015-04-20 01:05:34 -0400

    Installed mediaelement and jquery, through bower. Added bower tasks to copy mediaelement and jquery from the component directory. Organized mediaelement files into correct folders, added grunt task to replace image urls in the css to look at the correct directory.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,17 +3,32 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    /*build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }*/
+    bower: {
+      install: {
+        options: {
+          targetDir: './assets/vendor',
+          layout: 'byComponent',
+          cleanTargetDir: true,
+        }
+      }
+    },
+    replace: {
+      img_urls: {
+        src: ['./assets/vendor/mediaelement/css/*.css'],
+        overwrite: true,
+        replacements: [{
+          from: 'url(',
+          to: 'url(../img/'
+        }]
+      }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
-  //grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-bower-task');
+  grunt.loadNpmTasks('grunt-text-replace');
 
   // Default task(s).
-  //grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['bower:install','replace:img_urls']);
 
 };
