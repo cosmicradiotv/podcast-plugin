594ccf735f116cf4c8274806f480d2cc01252475
Matthew Fraser <blendermf@gmail.com>
2015-04-19 23:10:27 -0400

    Added package management tools
    Added package.json, bower.json.
    Installed mediaelement, jquery and grunt, added as dependencies
    Updated .gitignore to include node_modules and bower_components

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    /*build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }*/
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  //grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  //grunt.registerTask('default', ['uglify']);
+
+};
